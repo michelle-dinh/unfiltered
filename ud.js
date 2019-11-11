82b46e80faa3f93ae3042be67ac7ff062b553875
Michelle Dinh <michelle.dinh96@yahoo.com>
2019-11-10 22:18:45 -0800

    Update ud.js

--- ud.js
+++ ud.js
@@ -87,7 +87,7 @@ term[11] = "<b><center>feels</center></b>" + "<br>" +
 term[12] = "<b><center>tbh</center></b>" + "<br>" + 
 "<h6>Tbh - to be honest. Is fucking overused and when ugly ass bitches post on their Instagram 'comment for a tbh!' <br>" + 
 "It means that they are gonna write some complete bullshit about how nice you <br> when they don't even give two fucks.... And then the next day you find out that motherfucker ain't even following you back.</h6>" + 
-"<h6>[Ex.] <i><b>Tbh</b> your so friendly and I love how you care so much about equality :)</i></h6>" +
+"<h6>[Ex.] <i><b>Tbh</b> you're so friendly and I love how you care so much about equality :)</i></h6>" +
 "<h6>entry by Bitch peas [2016]</h6>";
 
 term[13] = "<b><center>gucci</center></b>" + "<br>" + 
@@ -128,34 +128,78 @@ term[19] = "<b><center>yas</center></b>" + "<br>" +
 
 term[20] = "<b><center>extra</center></b>" + "<br>" + 
 "<h6>over the top <br> excessive, dramatic behavior <br> way too much</h6>" + 
-"<h6>[Ex.] <i>Guy: Hey do you know that dude over there? <br> Girl: <b>YASSSS</b> he's so awesome like I mean <b>YASSSS</b>.</i></h6>" +
-"<h6>entry by WingsDude [2014]</h6>";
-// term[21] = "extra";
-// term[22] = "gucci";
-// term[23] = "keep it 100";
-// term[24] = "lit";
-// term[25] = "low key";
-// term[26] = "ratchet";
-// term[27] = "savage";
-// term[28] = "stan";
-// term[29] = "thirsty";
-// term[30] = "AF";
-// term[31] = "turnt";
-// term[32] = "twerk";
-// term[33] = "milk";
-// term[34] = "woke";
-// term[35] = "finsta";
-// term[36] = "simping";
-// term[37] = "roast";
-// term[38] = "high key";
-// term[39] = "feels";
-// term[40] = "goals";
-// term[41] = "5 AM";
-// term[42] = "aight"
-// term[43] = "and i oop"
-// term[44] = "vsco girl"
-// term[45] = "FOMO"
+"<h6>[Ex.] <i>Robert's behavior was <b>extra</b> when he refused to accept Anna's apology.</i></h6>" +
+"<h6>entry by Anonymous [2003]</h6>";
 
+term[21] = "<b><center>low-key</center></b>" + "<br>" + 
+"<h6>1. quiet, modest, not very assertive <br> 2. Desperate</h6>" + 
+"<h6>[Ex.] <i>They rented a <b>low-key</b> dorm for their meetings.</i></h6>" +
+"<h6>entry by They Return of Light Joker [2009]</h6>";
+
+term[22] = "<b><center>thirsty</center></b>" + "<br>" + 
+"<h6>1. Too eager to get something (especially play) <br> 2. to treat with little emphasis</h6>" + 
+"<h6>[Ex.] <i>(Boy running up): Ay gurl whasup? Look, you lookin real nice, can I get that young number? <br> (Girl under breath): <b>Thirsty</b></i></h6>" +
+"<h6>entry by Giddy [2003]</h6>";
+
+term[23] = "<b><center>roast</center></b>" + "<br>" + 
+"<h6>To tell someone off with a bad ass comeback.</h6>" + 
+"<h6>[Ex.] <i><b>Roast</b> <br> Boy: kill yourself <br>Girl: if I wanted to kill myself I'd climb your Ego and jump to your IQ</i></h6>" +
+"<h6>entry by Nikki Razbear [2016]</h6>";
+
+term[25] = "<b><center>tilted</center></b>" + "<br>" + 
+"<h6>When you're getting slightly mad at someone/something.</h6>" + 
+"<h6>[Ex.] <i>hey Arteezy, how's your team treating you? <br> i'm getting <b>tilted</b> already by this bullshit chen</i></h6>" +
+"<h6>entry by tsonkata [2016]</h6>";
+
+term[26] = "<b><center>savage</center></b>" + "<br>" + 
+"<h6>A savage is some who does not care about the consequences of his or her actions. Usually the savage will do things that make other people say, 'What the fuck are you crazy?'</h6>" + 
+"<h6>[Ex.] <i>Mike Tyson is a straight up <b>Savage</b>.</i></h6>" +
+"<h6>entry by B-dub (refers to my age) [2005]</h6>";
+
+term[27] = "<b><center>stan</center></b>" + "<br>" + 
+"<h6>A crazed and or obsessed fan. The term comes from the song Stan by eminem. The term Stan is used to describe a fan who goes to great lengths to obsess over a celebrity.</h6>" + 
+"<h6>[Ex.] <i>Person 1: Dude these kids have been waiting outside the concert since 8 A:M even though no one was lined up... <br> Person 2: What a bunch of <b>Stans</b></i></h6>" +
+"<h6>entry by I Chink Im Cool [2012]</h6>";
+
+term[28] = "<b><center>goals</center></b>" + "<br>" + 
+"<h6>What dumbass girls say to each other on Instagram when someone posts a pic that is very attractive or is of something they aspire to, like a relationship. It's basically a way of sucking up to someone and it's almost as annoying and shallow as when girls post shit like 'omg stop being so perf!' and then of course the required response is 'no that's you bb 😘' and it goes back and forth like that. Both examples are typically found thoroughly nauseating by non-sheeple types and those who don't seek validation via Instagram likes</h6>" + 
+"<h6>[Ex.] <i>Person 1: Dude these kids have been waiting outside the concert since 8 A:M even though no one was lined up... <br> Person 2: What a bunch of <b>Stans</b></i></h6>" +
+"<h6>entry by elyxium [2015]</h6>";
+
+term[29] = "<b><center>simping</center></b>" + "<br>" + 
+"<h6>when someones in their feelings aka when they're sad</h6>" + 
+"<h6>[Ex.] <i>ally: ni**a why you playing that sad music for? <br> dude: shut up ally, im <b>simping</b></i></h6>" +
+"<h6>entry by dirty dej [2016]</h6>";
+
+term[30] = "<b><center>alright</center></b>" + "<br>" + 
+"<h6>saying alright but in a hip and ghetto way</h6>" + 
+"<h6>[Ex.] <i>'Clean the car,' she said. <br> '<b>Aight</b>.'</i></h6>" +
+"<h6>entry by ShexxiBaby [2003]</h6>";
+
+term[31] = "<b><center>finna</center></b>" + "<br>" + 
+"<h6>Abbreviation of 'fixing to'. Normally means 'going to'.</h6>" + 
+"<h6>[Ex.] <i>I'm <b>finna</b> go da' sto!</i></h6>" +
+"<h6>entry by TimmyT [2003]</h6>";
+
+term[32] = "<b><center>finsta</center></b>" + "<br>" + 
+"<h6>A spam Instagram account where people post what they are too afraid to post on the real account</h6>" + 
+"<h6>[Ex.] <i>Did you see Chris's <b>finsta</b>? It is so funny.</i></h6>" +
+"<h6>entry by rantmonster2319 [2016]</h6>";
+
+term[33] = "<b><center>5 AM</center></b>" + "<br>" + 
+"<h6>A sign that it's time to go to bed. Goodnight.</h6>" + 
+"<h6>[Ex.] <i>Ah, jeez, <b>5 AM</b> already? I'm outta here.</i></h6>" +
+"<h6>entry by MarComiX [2009]</h6>";
+
+term[34] = "<b><center>ship</center></b>" + "<br>" + 
+"<h6>Noun: Short for romantic relationship, popularized in fanfiction circles. <br> Verb: To endorse a romantic relationship.</h6>" + 
+"<h6>[Ex.] <i>I <b>ship</b> Ron/Hermione</i></h6>" +
+"<h6>entry by magwana [2003]</h6>";
+
+term[35] = "<b><center>get your mans</center></b>" + "<br>" + 
+"<h6>A warning in which you let someone know to tell the person they are with to either relax or to stop acting in an irrational manner</h6>" + 
+"<h6>[Ex.] <i>Hey Kyle why is ya boy akin up, <b>get your mans</b> bro</i></h6>" +
+"<h6>entry by SydMak [2019]</h6>";
 
 function urbanTerm() {
 	var randomTerm = Math.floor(Math.random()*(term.length));
